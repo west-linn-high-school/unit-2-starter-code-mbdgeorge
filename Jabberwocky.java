c3c5f6fc0ceb6c3d811db97d3f395eb8cec2b8aa
Micahel George <mbdgeorge@gmail.comm>
2019-09-06 13:01:04 -0700

    Added third line of first stanza

--- Jabberwocky.java
+++ Jabberwocky.java
@@ -9,6 +9,7 @@ public class Jabberwocky
 {
     public static void main(String[] args){
         System.out.print("'Twas brillig, and the slithy toves/\n");
-        System.out.print("Did gyre and gimble in the wabe");
+        System.out.print("Did gyre and gimble in the wabe\n");
+        System.out.print("All mimbsy and the boro groves.");
     }
 }
