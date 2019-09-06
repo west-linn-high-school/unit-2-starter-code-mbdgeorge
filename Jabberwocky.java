950d91ee38a5822f934280efd3bb4a0829b3998b
Micahel George <mbdgeorge@gmail.comm>
2019-09-06 12:58:26 -0700

    Added Jabberywocky class

---
+++ Jabberwocky.java
@@ -0,0 +1,14 @@
+
+/**
+ * The first stanza of the Jabberwocky poem
+ *
+ * @author George
+ * @version 1.0
+ */
+public class Jabberwocky
+{
+    public static void main(String[] args){
+        System.out.print("'Twas brillig, and the slithy toves/\n");
+        System.out.print("Did gyre and gimble in the wabe");
+    }
+}
